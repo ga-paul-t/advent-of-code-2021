5bb7f6c136fe5409374a940ad3498b517db01a50
paul.t <paul.t@gembaadvantage.com>
2021-12-09 06:03:26 +0000

    include test

--- go/internal/aoc/day01a/task_test.go
+++ go/internal/aoc/day01a/task_test.go
@@ -1,6 +1,29 @@
 package day01a
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPuzzle_WithSample(t *testing.T) {
+	// Sample data from: https://adventofcode.com/2021/day/1
+	input = `199
+200
+208
+210
+200
+207
+240
+269
+260
+263`
+
+	puz := Puzzle{}
+	res := puz.Run()
+
+	assert.Equal(t, 7, res)
+}
 
 func Benchmark_Run(b *testing.B) {
 	p := Puzzle{}
