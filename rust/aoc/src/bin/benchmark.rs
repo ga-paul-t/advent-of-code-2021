8d5065df38c858ee12544f6289749f1cfb89cd35
paul.t <paul.t@gembaadvantage.com>
2021-12-14 17:01:27 +0000

    capture total elapsed time in rust

--- rust/aoc/src/bin/benchmark.rs
+++ rust/aoc/src/bin/benchmark.rs
@@ -1,9 +1,11 @@
+use std::ops::Add;
+use std::time::Duration;
 use took::{Timer, Took};
 
-const RUNS: usize = 100;
+const RUNS: usize = 200;
 
 fn main() {
-    println!("🎄 Advent of Code 2021 - Benchmark");
+    println!("🎄 Advent of Code 2021 - Benchmark [executions: {}]", RUNS);
     println!();
 
     let executions: Vec<_> = aoc::jobs()
@@ -21,5 +23,11 @@ fn main() {
 
     executions.iter().for_each(|exc| {
         println!("🧩 Puzzle {}: {:<14} [time taken: {}]", exc.0, exc.1, Took::from_std(exc.2))
-    })
+    });
+
+    let total_elapsed = executions.iter()
+        .fold(Duration::new(0,0), |total, exec | total.add(exec.2));
+
+    println!();
+    println!("Total elapsed time: [{}]", Took::from_std(total_elapsed));
 }
--- rust/aoc/src/bin/runner.rs
+++ rust/aoc/src/bin/runner.rs
@@ -1,13 +1,23 @@
-use took::Timer;
+use std::ops::Add;
+use std::time::Duration;
+use took::{Timer, Took};
 
 fn main() {
     println!("🎄 Advent of Code 2021");
     println!();
 
+    let mut total_elapsed = Duration::new(0, 0);
     aoc::jobs()
         .iter()
         .for_each(|job| {
             let now = Timer::new();
-            println!("🧩 Puzzle {}: {:<14} [time taken: {}]", job.1, job.0(), now.took().to_string())
+            let result = job.0();
+            let taken = now.took().into_std();
+            println!("🧩 Puzzle {}: {:<14} [time taken: {}]", job.1, result, Took::from_std(taken));
+
+            total_elapsed = total_elapsed.add(taken);
         });
+
+    println!();
+    println!("Total elapsed time: [{}]", Took::from_std(total_elapsed));
 }
